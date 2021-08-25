c60a42f0bc386482a68ca3c3f709638115d7d2ee
Hariharan <64999646+Hariharan-2711@users.noreply.github.com>
2021-08-25 12:53:12 +0530

    Update uart.v

--- uart.v
+++ uart.v
@@ -13,3 +13,5 @@ module uart(pc_in_t, clk_t_in, clk_r_in, ok);
      fifo_tx ft(.clk_fifo_tx(clk_t_in), .data_in(pc_in_t), .next_frame(dma_txend_in), .data_out(data), .fifo_tx_status(status));
      transmitter t(.pc_t(data), .clk(clk_t_in), .dma_txend(dma_txend_in), .tx(tx_in), .fifo_status(status));
      receiver r(.rx(tx_in), clk_r(clk_r_in), .dma_rxend());
+     
+endmodule
